79361d98a0a9ffd85737e81e608fa45d6428c3b1
PlasmolysisMango <mrgrandpa@163.com>
2025-11-27 02:21:39 +0800

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"image"
+	_ "image/jpeg"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -9,6 +12,7 @@ import (
 
 type Game struct {
 	posX, posY int
+	img        *ebiten.Image
 }
 
 func (g *Game) Update() error {
@@ -23,6 +27,15 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrintAt(screen, "Hello World", g.posX, g.posY)
+	geom := ebiten.GeoM{}
+	geom.Scale(0.2, 0.2)
+	geom.Rotate(0.5)
+	op := &ebiten.DrawImageOptions{
+		GeoM:   geom,
+		Filter: ebiten.FilterNearest,
+	}
+	// 可以在这里设置op.GeoM进行位置、旋转等变换
+	screen.DrawImage(g.img, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -31,13 +44,30 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 var _ ebiten.Game = &Game{}
 
-type Sprite struct {
+func loadJpegImage(path string) (*ebiten.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return ebiten.NewImageFromImage(img), nil
 }
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetTPS(30)
+	img, err := loadJpegImage("asset/pics/hotal.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ebiten.RunGame(&Game{img: img}); err != nil {
 		log.Fatal(err)
 	}
 }
